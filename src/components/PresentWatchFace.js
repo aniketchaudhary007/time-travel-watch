3cddd0f2dfda1b0ec2fe3d93e38999c6f1ba0dc8
Aniket Chaudhary <aniket@d-centralize.nl>
2022-05-03 14:14:36 +0530

    create PresentYearWatchFace

--- src/components/PresentWatchFace.js
+++ src/components/PresentWatchFace.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import * as PropTypes from 'prop-types';
 import OuterWrapper from './styles/OuterWrapper';
 import InnerWrapper from './styles/InnerWrapper';
 import DateWrapper from './styles/DateWrapper';
 import TimeWrapper from './styles/TimeWrapper';
-import JumpButton from './styles/JumpButton';
+import Button from './styles/Button';
 import VectorImage from '../images/vector.svg';
 import TimezoneImage from '../images/timezoneIcon.svg';
 import ButtonIcon from './styles/ButtonIcon';
-import TimezoneButton from './styles/TimezoneButton';
 import ButtonWrapper from './styles/ButtonWrapper';
+import TimezoneWrapper from './styles/TimezoneWrapper';
 
-function PresentWatchFace({ time, date }) {
+function PresentWatchFace({
+  time, date, timezone, renderSelectTimezoneScreen, renderSelectYearScreen,
+}) {
   return (
     <OuterWrapper>
       <InnerWrapper>
@@ -20,19 +23,30 @@ function PresentWatchFace({ time, date }) {
         <DateWrapper>
           {date}
         </DateWrapper>
+        <TimezoneWrapper>
+          {timezone}
+        </TimezoneWrapper>
       </InnerWrapper>
       <ButtonWrapper>
-        <JumpButton>
+        <Button onClick={renderSelectYearScreen}>
           <ButtonIcon src={VectorImage} alt="jump" />
           Jump
-        </JumpButton>
-        <TimezoneButton>
+        </Button>
+        <Button onClick={renderSelectTimezoneScreen}>
           <ButtonIcon src={TimezoneImage} alt="timezone" />
           Timezone
-        </TimezoneButton>
+        </Button>
       </ButtonWrapper>
     </OuterWrapper>
   );
 }
+
+PresentWatchFace.propTypes = {
+  time: PropTypes.number.isRequired,
+  timezone: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  renderSelectYearScreen: PropTypes.func.isRequired,
+  renderSelectTimezoneScreen: PropTypes.func.isRequired,
+};
 
 export default PresentWatchFace;
