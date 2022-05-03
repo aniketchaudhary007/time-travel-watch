2c9bddda9d4e5c796922e34c1f0cd7bc00fb9f80
Aniket Chaudhary <aniket@d-centralize.nl>
2022-05-03 14:13:35 +0530

    added timezone and jump functionality

--- src/App.js
+++ src/App.js
@@ -4,66 +4,146 @@ import { WATCH_FORMAT } from './const';
 import PresentWatchFace from './components/PresentWatchFace';
 import FutureYearWatchFace from './components/FutureYearWatchFace';
 import PastYearWatchFace from './components/PastYearWatchFace';
+import SelectTimezone from './components/SelectTimezone';
+import SelectYear from './components/SelectYear';
 import 'moment-timezone';
+
+const LOCAL_TIMEZONE = moment.tz.guess();
 
 function App() {
   const [time, setTime] = useState(0);
   const [date, setDate] = useState('');
-  //  const [timezone, setTimezone] = useState('');
-  const [watchFace, setWatchFace] = useState('LOCALTIME');
-
-  const currentTime = () => {
-    setTime(moment().format('h:mm:ss A'));
-    setDate(moment().format('MMMM Do YYYY'));
-    setWatchFace('LOCALTIME');
-  };
+  const [year, setYear] = useState(moment().year());
+  const [selectTimezone, setSelectTimezone] = useState(false);
+  const [selectYear, setSelectYear] = useState(false);
+  const [timezone, setTimezone] = useState(LOCAL_TIMEZONE);
+  const [watchFace, setWatchFace] = useState(WATCH_FORMAT.PRESENT_TIME);
+  let second = 0;
 
   useEffect(() => {
     const interval = setInterval(() => {
-      currentTime();
+      // Reset the clock after every 15 seconds.
+      if (second && second % 15 === 0) {
+        resetTime();
+        second = 0;
+      }
+
+      if (watchFace === WATCH_FORMAT.PRESENT_TIME) {
+        setPresentTime();
+      } else if (watchFace === WATCH_FORMAT.FUTURE_TIME) {
+        setFutureTime();
+      } else {
+        setPastTime();
+      }
+      second += 1;
     }, 1000);
     return () => clearInterval(interval);
-  }, []);
+  }, [timezone, watchFace]);
 
-  // const handleChange = (event) => {
-  //   setValue(event.target.value);
-  // };
+  const updateWatchFaceAccToYear = () => {
+    const presentYear = moment().year();
 
-  const newTimezone = () => {
-    setTime(moment().tz('America/Los_Angeles').format('h:mm:ss A'));
-    setDate(moment().tz('America/Los_Angeles').format('MMMM Do YYYY'));
-    setWatchFace('TIMEZONE');
+    if (presentYear > year) {
+      setWatchFace(WATCH_FORMAT.PAST_TIME);
+    } else if (presentYear < year) {
+      setWatchFace(WATCH_FORMAT.FUTURE_TIME);
+    }
+    renderWatchScreen();
   };
 
-  const futureYear = () => {
-    setDate(moment().add(1, 'Y').format('MMMM Do YYYY'));
-    setWatchFace('FUTURE_YEAR');
+  const setPresentTime = () => {
+    setTime(moment().tz(timezone).format('h:mm:ss A'));
+    setDate(moment().tz(timezone).format('ddd, MMMM Do YYYY'));
   };
 
-  const pastYear = () => {
-    setDate(moment().subtract(1, 'Y').format('MMMM Do YYYY'));
-    setWatchFace('PAST_YEAR');
+  const setFutureTime = () => {
+    const numberOfYears = year - moment().year();
+    setTime(moment().tz(timezone).format('h:mm:ss A'));
+    setDate(moment().add(numberOfYears, 'Y').format('ddd, MMMM Do YYYY'));
   };
+
+  const setPastTime = () => {
+    const numberOfYears = moment().year() - year;
+    setTime(moment().tz(timezone).format('h:mm:ss A'));
+    setDate(moment().subtract(numberOfYears, 'Y').format('ddd, MMMM Do YYYY'));
+  };
+
+  const renderSelectTimezoneScreen = () => {
+    setSelectTimezone(true);
+  };
+
+  const renderSelectYearScreen = () => {
+    setSelectYear(true);
+  };
+
+  const renderWatchScreen = () => {
+    setSelectTimezone(false);
+    setSelectYear(false);
+  };
+
+  const handleYearChange = (event) => {
+    setYear(Number(event.target.value));
+  };
+
+  const updateTimezoneinWatch = (tz) => {
+    setTimezone(tz);
+    renderWatchScreen();
+  };
+
+  const resetTime = () => {
+    renderWatchScreen();
+    setWatchFace(WATCH_FORMAT.PRESENT_TIME);
+    setTimezone(LOCAL_TIMEZONE);
+  };
+
+  // Select the Timezone
+  if (selectTimezone) {
+    return (
+      <SelectTimezone
+        timezone={timezone}
+        renderWatchScreen={renderWatchScreen}
+        updateTimezoneinWatch={updateTimezoneinWatch}
+      />
+    );
+  }
+
+  // Select the year to jump in past or future
+  if (selectYear) {
+    return (
+      <SelectYear
+        year={year}
+        renderWatchScreen={renderWatchScreen}
+        updateWatchFaceAccToYear={updateWatchFaceAccToYear}
+        handleYearChange={handleYearChange}
+      />
+    );
+  }
 
   return (
     <div>
-      {watchFace === WATCH_FORMAT.LOCALTIME && (
+      {watchFace === WATCH_FORMAT.PRESENT_TIME && (
         <PresentWatchFace
-          time={time}
+          timezone={timezone}
           date={date}
-
+          time={time}
+          renderSelectTimezoneScreen={renderSelectTimezoneScreen}
+          renderSelectYearScreen={renderSelectYearScreen}
         />
       )}
-      {watchFace === WATCH_FORMAT.FUTURE_YEAR && (
+      {watchFace === WATCH_FORMAT.FUTURE_TIME && (
         <FutureYearWatchFace
           time={time}
           date={date}
+          timezone={timezone}
+          resetTime={resetTime}
         />
       )}
-      {watchFace === WATCH_FORMAT.PAST_YEAR && (
+      {watchFace === WATCH_FORMAT.PAST_TIME && (
         <PastYearWatchFace
           time={time}
           date={date}
+          timezone={timezone}
+          resetTime={resetTime}
         />
       )}
     </div>
